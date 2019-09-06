f63e3b76dda7699282a012a415bd7312d61c28c8
yirui wang <yirui.wang@unity3d.com>
2019-09-06 11:42:23 +0800

    Update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init(){
+
+        for(int i = 0; i < 4; ++i){
+            for(int j = 0; j < 4; ++j){
+                CreateSprite();
+            }
+        }
+    }
+
+    private void CreateSprite(){
+
+    }
+}
