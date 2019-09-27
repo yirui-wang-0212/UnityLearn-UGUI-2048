e2c17ae65559a6489c555239cc157c315beced04
yirui wang <ygritte_wang@163.com>
2019-09-27 17:36:55 +0800

    Update

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour
         Init();
     }
 
-    // 初始化
+    // 初始化，创建 4 * 4 的  Sprite
     private void Init(){
 
         for(int r = 0; r < 4; ++r){
@@ -29,11 +29,14 @@ public class GameController : MonoBehaviour
         GameObject go = new GameObject(r.ToString() + c.ToString());
         // 2. 添加 Image 组件
         go.AddComponent<Image>();
-        // NumberSprite 脚本： Sprite 的行为
-        NumberSprite action = go.AddComponent<NumberSprite>(); // Awake 立即执行（创建之后立即执行），Start 下一阵帧执行
-        // 设置初始图片
+        // 在 4 * 4 的 GameObject 上添加 NumberSprite 脚本组件：用于 Sprite 的行为
+        // Awake 立即执行（创建之后立即执行），Start 下一帧执行，
+        // 因为在当前脚本中的 Start 会调用当前函数，并且在下一行的 action.SetImage(0); 会访问 NumberSprite 中的 image 并对其进行修改，
+        // 所以在 NumberSprite.cs 中 image = GetComponent<Image>(); 应该写在 Awake 中 而不是 Starr 中。
+        NumberSprite action = go.AddComponent<NumberSprite>(); 
+        // 设置初始图片，没有数字的图片
         action.SetImage(0);
-        // 3. 设置当前 GameObject 为创建对象的父节点
+        // 3. 设置当前 GameObject：GameController 为创建对象的父节点
         // 创建的游戏对象时 scale 默认为 1，false 表示不使用世界坐标，1 表示相对于父节点
         go.transform.SetParent(this.transform, false);
     }
